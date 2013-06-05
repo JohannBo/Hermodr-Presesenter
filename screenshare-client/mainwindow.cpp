c883323f5825587aea2d47f2cb1103db97dd9861
Johann Bornholdt <johann.bornholdt@gmail.com>
2013-06-05 21:53:33 +0200

    added experimental sound support

--- screenshare-client/mainwindow.cpp
+++ screenshare-client/mainwindow.cpp
@@ -2,14 +2,15 @@
 #include "ui_mainwindow.h"
 #include "dialoglogin.h"
 
-#include <QThread>
 #include <QScreen>
-#include <QPixmap>
 #include <QBuffer>
 #include <QCursor>
-#include <QJsonDocument>
-#include <QJsonArray>
-#include <QJsonObject>
+#include <QFile>
+#include <QPainter>
+#include <QMessageBox>
+#include <QProcess>
+
+
 
 const int MainWindow::FRAME_LENGTH = 1000;
 
@@ -18,36 +19,41 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-//    DialogLogin l;
-//    int login = l.exec();
-    xratio = 1.0;
-    yratio = 1.0;
 
-    for (int i = 0; i < GRID_SIZE; ++i) {
-        for (int j = 0; j < GRID_SIZE; ++j) {
-            images[i][j] = "";
-        }
-    }
-    oldCursorX = 0;
-    oldCursorY = 0;
+    screenshotTimer = new QTimer(this);
+    isScreenshotTimer = true;
+    cursorTimer = new QTimer(this);
+    isCursorTimer = true;
+    audioTimer = new QTimer(this);
+    isAudioTimer = true;
 
-    wsSocket = new QWsSocket( this );
-    socketStateChanged( wsSocket->state() );
-
-    QObject::connect( wsSocket, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(socketStateChanged(QAbstractSocket::SocketState)) );
-    QObject::connect( wsSocket, SIGNAL(frameReceived(QString)), this, SLOT(displayMessage(QString)) );
-    QObject::connect( wsSocket, SIGNAL(connected()), this, SLOT(socketConnected()) );
-    QObject::connect( wsSocket, SIGNAL(disconnected()), this, SLOT(socketDisconnected()) );
-    connectSocket();
+    //    DialogLogin l;
+    //    int login = l.exec();
 
 }
 
 MainWindow::~MainWindow()
 {
-    delete wsSocket;
-    delete screenshotTimer;
-    delete ui;
+    if (isAudioRecorderOffline) {
+        delete audioRecorderOffline;
+    }
+    if (isAudioRecorderOnline) {
+        delete audioRecorderOnline;
+    }
+    if (isAudioTimer) {
+        delete audioTimer;
+    }
+    if (isCursorTimer) {
+        delete cursorTimer;
+    }
+    if (isScreenshotTimer) {
+        delete screenshotTimer;
+    }
+    if (isWsSocket) {
+        delete wsSocket;
+    }
 
+    delete ui;
 }
 
 void MainWindow::socketStateChanged(QAbstractSocket::SocketState socketState)
@@ -81,7 +87,7 @@ void MainWindow::socketStateChanged(QAbstractSocket::SocketState socketState)
     }
 }
 
-void MainWindow::displayMessage(QString message)
+void MainWindow::displayMessage(const QString message)
 {
     qDebug() << "message: " << message;
 }
@@ -89,6 +95,8 @@ void MainWindow::displayMessage(QString message)
 void MainWindow::socketConnected()
 {
     qDebug() << "socket connected.";
+    startScreenshare();
+
 }
 
 void MainWindow::socketDisconnected()
@@ -98,64 +106,96 @@ void MainWindow::socketDisconnected()
 
 void MainWindow::connectSocket()
 {
-    QUrl url("ws://127.0.0.1:9000/presenter?presenter=presenter1");
+
+    QUrl url("ws://" + ui->lineEdit_settings_serverUrl->text() + "/presenter?presenter=" + ui->lineEdit_settings_userName->text());
     wsSocket->connectToHost(url);
 }
 
-void MainWindow::on_pushButton_newScreenshare_start_clicked()
+void MainWindow::startScreenshare()
 {
-    screenshotTimer = new QTimer(this);
-    connect(screenshotTimer, SIGNAL(timeout()), this, SLOT(sendScreenshot()));
+    // todo schöner
+    if (ui->checkBox_newScreenshare_live->isChecked()) {
+        if (ui->checkBox_newScreenshare_live_audio->isChecked()) {
+            connect(audioTimer, SIGNAL(timeout()), this, SLOT(stopRecorderOnline()));
+            startRecorderOnline();
+        }
+    }
+
+    if (ui->checkBox_newScreenshare_file->isChecked()) {
+        if (ui->checkBox_newScreenshare_file_audio->isChecked()) {
+            startRecorderOffline();
+        }
+    }
+    connect(screenshotTimer, SIGNAL(timeout()), this, SLOT(takeScreenshot()));
     screenshotTimer->start(500);
 
-    cursorTimer = new QTimer(this);
     connect(cursorTimer, SIGNAL(timeout()), this, SLOT(sendCursorPosition()));
     cursorTimer->start(200);
 
 
+
+
 }
 
-void MainWindow::sendScreenshot()
+void MainWindow::takeScreenshot()
 {
-    QPixmap pixmap;
+    QPixmap screenshot;
 
     QScreen *screen = QGuiApplication::primaryScreen();
 
-    pixmap = screen->grabWindow(0, 0, 0, 1920, 1080);
+    // todo werte aus settings
+    int posX = ui->lineEdit_settings_positionX->text().toInt();
+    int posY = ui->lineEdit_settings_positionY->text().toInt();
+    int screenshotHeight = ui->lineEdit_settings_height->text().toInt();
+    int screenshotWidth = ui->lineEdit_settings_width->text().toInt();
+    int targetHeight = ui->lineEdit_settings_targetHeight->text().toInt();
 
-    int xold = pixmap.width();
-    int yold = pixmap.height();
+    screenshot = screen->grabWindow(0, posX, posY, screenshotWidth, screenshotHeight);
 
-    pixmap = pixmap.scaledToHeight(720, Qt::SmoothTransformation);
+    int xold = screenshot.width();
+    int yold = screenshot.height();
 
-    xratio = pixmap.width() / (double)xold;
-    yratio = pixmap.height() / (double)yold;
+    screenshot = screenshot.scaledToHeight(targetHeight, Qt::SmoothTransformation);
 
-    splitImage(pixmap);
-}
+    xratio = screenshot.width() / (double)xold;
+    yratio = screenshot.height() / (double)yold;
 
-void MainWindow::sendCursorPosition()
-{
-    int x = (double)QCursor::pos().x() * xratio;
-    int y = (double)QCursor::pos().y() * yratio;
-    if (x != oldCursorX || y != oldCursorY) {
-        QString sendval ="{\"type\" : \"cursor\",\"x\" : \"" + QString::number(x) + "\",\"y\" : \"" + QString::number(y) + "\"}";
-        wsSocket->write(sendval);
-        oldCursorX = x;
-        oldCursorY = y;
+    if (ui->checkBox_newScreenshare_file->isChecked()) {
+        saveScreenshot(screenshot);
     }
+    if (ui->checkBox_newScreenshare_live->isChecked()) {
+        sendScreenshot(screenshot);
+    }
+
+
 }
 
-void MainWindow::splitImage(QPixmap image){
-    int partX = image.width() / GRID_SIZE;
-    int partY = image.height() / GRID_SIZE;
+void MainWindow::saveScreenshot(QPixmap screenshot)
+{
+    // draw mouse into screenshot
+    QPainter qp(&screenshot);
+    qp.fillRect(oldCursorX, oldCursorY, 5, 5, Qt::black);
+    qp.fillRect(oldCursorX+1, oldCursorY+1, 3, 3, Qt::white);
+    qp.end();
+
+    // save screenshot to file
+    QString filename = "pic" + QString::number(pictureFileIndex++) + ".jpg";
+    QFile myFile(filename);
+    myFile.open(QIODevice::WriteOnly);
+    screenshot.save(&myFile, "JPG");
+}
+
+void MainWindow::sendScreenshot(const QPixmap screenshot)
+{
+    int partX = screenshot.width() / GRID_SIZE;
+    int partY = screenshot.height() / GRID_SIZE;
     int posX = 0;
     int posY = 0;
 
 
     for (int x = 0; x < GRID_SIZE; ++x) {
         for (int y = 0; y < GRID_SIZE; ++y) {
-            QPixmap imagePart = image.copy(posX, posY, partX, partY);
+            QPixmap imagePart = screenshot.copy(posX, posY, partX, partY);
             posX += partX;
 
             QString oldPart = images[x][y];
@@ -169,15 +209,34 @@ void MainWindow::splitImage(QPixmap image){
             if (newPart != oldPart) {
                 images[x][y] = newPart;
                 sendImage(newPart, x, y);
-
-//                qDebug() << "X: " << posX << " Y: " << posY << " width: " << imagePart.width() << " heigth: " << imagePart.height() << " length: " << newPart.length();
             }
         }
         posY += partY;
         posX = 0;
     }
+}
 
+void MainWindow::sendCursorPosition()
+{
+    int newCursorX = (QCursor::pos().x() - ui->lineEdit_settings_positionX->text().toInt()) * xratio;
+    int newCursorY = (QCursor::pos().y() - ui->lineEdit_settings_positionY->text().toInt() ) * yratio;
+    if (newCursorX != oldCursorX || newCursorY != oldCursorY) {
+        QString type = "cursor";
+        QString x = QString::number(newCursorX);
+        QString y = QString::number(newCursorY);
 
+        QString sendVal = "{"
+                "\"type\":\"" + type + "\","
+                "\"x\":\"" + x + "\","
+                "\"y\":\"" + y + "\""
+                "}";
+
+        if (ui->checkBox_newScreenshare_live->isChecked()) {
+            wsSocket->write(sendVal);
+        }
+        oldCursorX = newCursorX;
+        oldCursorY = newCursorY;
+    }
 }
 
 void MainWindow::sendImage(QString imagePart, int posX, int posY) {
@@ -209,15 +268,193 @@ void MainWindow::sendImage(QString imagePart, int posX, int posY) {
     }
 }
 
+void MainWindow::startRecorderOffline()
+{
+    QAudioEncoderSettings audioSettings;
+    audioSettings.setCodec("audio/vorbis");
+    audioSettings.setQuality(QMultimedia::NormalQuality);
+
+    audioRecorderOffline= new QAudioRecorder;
+    isAudioRecorderOffline = true;
+    audioRecorderOffline->setEncodingSettings(audioSettings);
+
+    audioRecorderOffline->setOutputLocation(QUrl::fromLocalFile("sound.ogg"));
+
+    audioRecorderOffline->record();
+}
+
+void MainWindow::startRecorderOnline()
+{
+    QAudioEncoderSettings audioSettings;
+    audioSettings.setCodec("audio/vorbis");
+    audioSettings.setQuality(QMultimedia::VeryLowQuality);
+
+    audioRecorderOnline = new QAudioRecorder;
+    isAudioRecorderOnline = true;
+    audioRecorderOnline->setEncodingSettings(audioSettings);
+
+    audioRecorderOnline->setOutputLocation(QUrl::fromLocalFile("sound" + QString::number(soundFileIndex++) + ".ogg"));
+
+//    audioTimer->singleShot(1000, this, SLOT(stopRecorderOnline()));
+    audioTimer->start(1000);
+    audioRecorderOnline->record();
+
+}
+
+void MainWindow::stopRecorderOnline()
+{
+    audioTimer->stop();
+    audioRecorderOnline->stop();
+    QUrl soundUrl = audioRecorderOnline->outputLocation();
+    delete audioRecorderOnline;
+    isAudioRecorderOnline = false;
+    QString sound = openSoundFile(soundUrl);
+    sendAudio(sound);
+    startRecorderOnline();
+
+}
+
+QString MainWindow::openSoundFile(QUrl url)
+{
+    QFile myFile(url.url());
+    myFile.open(QIODevice::ReadOnly);
+    QByteArray array = myFile.readAll();
+    myFile.close();
+    myFile.remove();
+    return array.toBase64();
+}
+
+void MainWindow::sendAudio(QString audio)
+{
+    while (audio.length() > 0) {
+        QString type = "audio";
+        QString last = "0";
+        QString partToSend = "";
+
+        if (audio.length() > FRAME_LENGTH) {
+            partToSend = audio.right(FRAME_LENGTH);
+            audio.chop(FRAME_LENGTH);
+        } else {
+            partToSend = audio;
+            audio = "";
+            last = "1";
+        }
+
+        QString sendVal = "{"
+                "\"type\":\"" + type + "\","
+                "\"last\":\"" + last + "\","
+                "\"data\":\"" + partToSend + "\""
+                "}";
+        wsSocket->write(sendVal);
+    }
+}
+
+void MainWindow::on_pushButton_newScreenshare_start_clicked()
+{
+    if (!ui->checkBox_newScreenshare_file->isChecked() && !ui->checkBox_newScreenshare_live->isChecked()) {
+        QMessageBox msgBox(this);
+        msgBox.setText("No Target has been chosen.");
+        msgBox.exec();
+        return;
+    }
+
+    ui->pushButton_newScreenshare_start->setEnabled(false);
+    ui->pushButton_newScreenshare_stop->setEnabled(true);
+    ui->pushButton_newScreenshare_pause->setEnabled(true);
+    ui->tab_settings->setEnabled(false);
+
+    xratio = 1.0;
+    yratio = 1.0;
+    oldCursorX = 0;
+    oldCursorY = 0;
+    pictureFileIndex = 0;
+    soundFileIndex = 0;
+
+    for (int x = 0; x < GRID_SIZE; ++x) {
+        for (int y = 0; y < GRID_SIZE; ++y) {
+            images[x][y] = "";
+        }
+    }
+
+    // Todo directory anlegen
+
+    if (ui->checkBox_newScreenshare_live->isChecked()) {
+        wsSocket = new QWsSocket( this );
+        isWsSocket = true;
+        socketStateChanged( wsSocket->state() );
+
+        connect( wsSocket, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(socketStateChanged(QAbstractSocket::SocketState)) );
+        connect( wsSocket, SIGNAL(frameReceived(QString)), this, SLOT(displayMessage(QString)) );
+        connect( wsSocket, SIGNAL(connected()), this, SLOT(socketConnected()) );
+        connect( wsSocket, SIGNAL(disconnected()), this, SLOT(socketDisconnected()) );
+        connectSocket();
+    } else {
+        startScreenshare();
+    }
 
 
+}
+
+void MainWindow::on_pushButton_newScreenshare_stop_clicked()
+{
+    QString program = ui->lineEdit_settings_ffmpeg->text();
+    QStringList arguments;
+    arguments << "-i" << "sound0.ogg" << "blubb.mp3";
+    QProcess *myProcess = new QProcess(this);
+    myProcess->start(program, arguments);
+
+    // todo ffmpeg
+
+    if (isCursorTimer) {
+        cursorTimer->stop();
+    }
+
+    if (isScreenshotTimer) {
+        screenshotTimer->stop();
+    }
+
+    if (isAudioTimer) {
+        audioTimer->stop();
+    }
+
+    if (isAudioRecorderOffline) {
+        audioRecorderOffline->stop();
+    }
+
+    if (isAudioRecorderOnline) {
+        audioRecorderOnline->stop();
+    }
+
+    if (isWsSocket) {
+        wsSocket->abort("disconnect");
+    }
 
 
+    ui->pushButton_newScreenshare_start->setEnabled(true);
+    ui->pushButton_newScreenshare_stop->setEnabled(false);
+    ui->pushButton_newScreenshare_pause->setEnabled(false);
+    ui->tab_settings->setEnabled(true);
+}
 
+void MainWindow::on_checkBox_newScreenshare_live_clicked()
+{
+    if (ui->checkBox_newScreenshare_live->isChecked()) {
+        ui->checkBox_newScreenshare_live_audio->setEnabled(true);
+    } else {
+        ui->checkBox_newScreenshare_live_audio->setEnabled(false);
+    }
+}
 
+void MainWindow::on_checkBox_newScreenshare_file_clicked()
+{
+    if (ui->checkBox_newScreenshare_file->isChecked()) {
+        ui->checkBox_newScreenshare_file_audio->setEnabled(true);
+    } else {
+        ui->checkBox_newScreenshare_file_audio->setEnabled(false);
+    }
+}
 
-
-
-
-
-
+void MainWindow::on_pushButton_newScreenshare_pause_clicked()
+{
+    qDebug() << "todo";
+}
--- screenshare-client/mainwindow.h
+++ screenshare-client/mainwindow.h
@@ -5,6 +5,8 @@
 #include <QDebug>
 #include <QTimer>
 #include <QPixmap>
+#include <QUrl>
+#include <QAudioRecorder>
 
 #include "QWsSocket.h"
 #include "dialoglogin.h"
@@ -26,31 +28,59 @@ public:
 
 protected slots:
     void socketStateChanged(QAbstractSocket::SocketState socketState);
-    void displayMessage(QString);
+    void displayMessage(const QString);
     void socketConnected();
     void socketDisconnected();
     void connectSocket();
     
 private slots:
     void on_pushButton_newScreenshare_start_clicked();
-    void sendScreenshot();
+    void startScreenshare();
+    void takeScreenshot();
+    void sendScreenshot(const QPixmap screenshot);
+    void saveScreenshot(QPixmap screenshot);
     void sendCursorPosition();
-    void splitImage(QPixmap image);
     void sendImage(QString imagePart, int posX, int posY);
 
+    void startRecorderOnline();
+    void stopRecorderOnline();
+    void startRecorderOffline();
+    QString openSoundFile(QUrl url);
+    void sendAudio(QString audio);
+
+    void on_pushButton_newScreenshare_stop_clicked();
+
+    void on_checkBox_newScreenshare_live_clicked();
+
+    void on_checkBox_newScreenshare_file_clicked();
+
+    void on_pushButton_newScreenshare_pause_clicked();
+
 protected:
-    QWsSocket *wsSocket;
+
 
 private:
     static const int FRAME_LENGTH;
     Ui::MainWindow *ui;
+    QWsSocket *wsSocket;
+    bool isWsSocket;
+    QAudioRecorder *audioRecorderOnline;
+    bool isAudioRecorderOnline;
+    QAudioRecorder *audioRecorderOffline;
+    bool isAudioRecorderOffline;
     QTimer *screenshotTimer;
+    bool isScreenshotTimer;
     QTimer *cursorTimer;
+    bool isCursorTimer;
+    QTimer *audioTimer;
+    bool isAudioTimer;
     double xratio;
     double yratio;
     QString images[GRID_SIZE][GRID_SIZE];
     int oldCursorX;
     int oldCursorY;
+    int pictureFileIndex;
+    int soundFileIndex;
 };
 
 #endif // MAINWINDOW_H
